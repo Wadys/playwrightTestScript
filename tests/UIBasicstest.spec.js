09183b3f597286c4623b442399243caa77f671d6
Wady <wadyprograming@gmail.com>
2026-02-27 17:41:10 -0600

    Updated example

--- tests/UIBasicstest.spec.js
+++ tests/UIBasicstest.spec.js
@@ -1,27 +1,32 @@
-const {test, expect} = require('@playwright/test');
-
+const {test, expect, page} = require('@playwright/test');
+const url = "https://rahulshettyacademy.com/loginpagePractise/";
+const userName = "rahulshettyacademy";
+const password = "Learning@830$3mK2";
+// const userNameLocator =  page.locator("#username")
+    // const passwordLocator = page.locator("#password")
+    // const signInBtnLocator = page.locator("#signInBtn")
+    // const cardTitlesLocator = page.locator(".card-body a");
 test('First Playwright Test', async ({browser}) =>{
 //Chrome - plugins/ cookies
     const context = await browser.newContext();
     const page = await context.newPage()
-    const userName = page.locator("#username")
-    const signInBtn = page.locator("#signInBtn")
-    const cardTitles = page.locator(".card-body a");
-    await page.goto("https://rahulshettyacademy.com/loginpagePractise/");
-    // css, xpath, text, id, class
-    await userName.fill("rahulshettyacademy");
-    await page.locator("[type = 'password']").fill("Learning");//Wrong Password
-    await signInBtn.click();
+    
+  //Chrome - plugins/ cookies
+    await page.goto(url);
+    // css, xpath, text, id, class    
+    await userNameLocator.fill(userName);
+    await passwordLocator.fill("Learning");//Wrong Password
+    await signInBtnLocator.click();
     console.log(await page.locator("[style*='block']").textContent());
     await expect(page.locator("[style*='block']")).toContainText("Incorrect username/password.");
-    await userName.fill("");
-    await userName.fill("rahulshettyacademy");
-    await page.locator("[type = 'password']").fill("Learning@830$3mK2");
-    await signInBtn.click();
-    console.log(await cardTitles.first().textContent()); //first Element
-    console.log(await cardTitles.nth(0).textContent()); //first and nth(0) are same
-    console.log(await cardTitles.last().textContent()); //last Element
-    const allTitles = await cardTitles.allTextContents(); //this onnly works because we ask for
+    await userNameLocator.fill("");
+    await userNameLocator.fill(userName);
+    await passwordLocator.fill(password);
+    await signInBtnLocator.click();
+    console.log(await cardTitlesLocator.first().textContent()); //first Element
+    console.log(await cardTitlesLocator.nth(0).textContent()); //first and nth(0) are same
+    console.log(await cardTitlesLocator.last().textContent()); //last Element
+    const allTitles = await cardTitlesLocator.allTextContents(); //this onnly works because we ask for
     // the previouse element if we handnt asked for the previouse element it will give us an error 
     // because it will not know which element we are talking about do to timeout
     console.log(allTitles);
@@ -33,4 +38,30 @@ test('Page Playwright Test', async ({page}) =>{
     //get tittle - assertion
     console.log(await page.title());
     await expect(page).toHaveTitle("Google");
+});
+
+test.only('UI Controls', async ({page}) => {
+    const userNameLocator = page.locator("#username");
+    const passwordLocator = page.locator("#password");
+    const signInBtnLocator = page.locator("#signInBtn");
+    const dropdownLocator = page.locator("select.form-control");
+    const checkbox = page.locator("#terms");
+    const radioBtn = page.locator(".radiotextsty");
+    await page.goto(url);
+    await userNameLocator.fill(userName);
+    await passwordLocator.fill(password);
+    await dropdownLocator.selectOption("consult");
+    await radioBtn.last().click();
+    await page.locator("#okayBtn").click();
+    console.log(await radioBtn.last().isChecked());
+    await expect(radioBtn.last()).toBeChecked();//Assertion for checkbox
+    await checkbox.check();
+    await expect(checkbox).toBeChecked();
+    console.log(await checkbox.isChecked());
+    await checkbox.uncheck();
+    await expect(checkbox).not.toBeChecked();
+    console.log(await checkbox.isChecked());
+    
+    // await signInBtn.click();
+    // await page.pause(); Waits and create a pause
 });
