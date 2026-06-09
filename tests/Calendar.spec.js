bd2a77a106653bd1ba10eef323c882b51d74ea98
Wady <wadyprograming@gmail.com>
2026-06-09 17:51:59 -0600

    Updated test cases

--- tests/Calendar.spec.js
+++ tests/Calendar.spec.js
@@ -3,15 +3,23 @@ const url = "https://rahulshettyacademy.com/seleniumPractise/#/offers";
 
 test('Calendar Validations', async({page}) => 
 {
-    const monthNumber = 12;
-    const date = 15;
-    const year = "2027";  
+    const monthNumber = "6";
+    const date = "15";
+    const year = "2027";
+    const expectedList = [monthNumber, date, year];  
     await page.goto(url);
     await page.locator(".react-date-picker__inputGroup__year").click();
     await page.locator(".react-calendar__navigation__label").click();
     await page.locator(".react-calendar__navigation__label__labelText").click();
     await page.getByText(year).click();
     await page.locator(".react-calendar__year-view__months__month").nth(monthNumber-1).click();
-    await page.locator(".react-calendar__month-view__days__day").nth(date+1).click();
-    await expect(page.getByText(monthNumber + " / " + date + " / " + year)).toBeVisible();
+    await page.locator(".react-calendar__month-view__days__day").nth(date).click();
+    
+    const inputs = page.locator('.react-date-picker__inputGroup__input');
+    
+    for (let i=0; i<expectedList.length; i++)
+    {
+        const value = await inputs.nth(i).inputValue();
+        expect(value).toEqual(expectedList[i]);
+    }
 })
